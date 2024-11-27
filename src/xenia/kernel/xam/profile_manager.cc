bcd441fd07af1ce87a2a7b0effde3642bd46fc4a
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-11-27 17:48:57 +0000

    [XAM] Maintain usage of constant XUserIndexAny

--- src/xenia/kernel/xam/profile_manager.cc
+++ src/xenia/kernel/xam/profile_manager.cc
@@ -402,7 +402,7 @@ uint8_t ProfileManager::FindFirstFreeProfileSlot() const {
       return i;
     }
   }
-  return -1;
+  return XUserIndexAny;
 }
 
 std::bitset<XUserMaxUserCount> ProfileManager::GetUsedUserSlots() const {
@@ -431,7 +431,7 @@ uint8_t ProfileManager::GetUserIndexAssignedToProfile(
 
     return index;
   }
-  return -1;
+  return XUserIndexAny;
 }
 
 std::filesystem::path ProfileManager::GetProfileContentPath(
